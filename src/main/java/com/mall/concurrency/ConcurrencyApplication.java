f031ddec0be73e6d20f64cfeeb37f4656212b64c
zhouheng <zhouheng>
2018-08-28 21:00:08 +0800

    Initial commit

---
+++ src/main/java/com/mall/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.mall.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+}
